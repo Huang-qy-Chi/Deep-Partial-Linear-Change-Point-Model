0f320080c4413817de51db10c2dcbd3b0c10473a
Huang-qy-Chi <79854798+Huang-qy-Chi@users.noreply.github.com>
2025-08-13 21:35:19 +0800

    seed.py

--- seed.py
+++ seed.py
@@ -2,11 +2,16 @@ import numpy as np
 import torch 
 import random
 def set_seed(seed):
+    """
+    This function fixes all random seeds to
+    ensure the reproducibility.
+    """
     np.random.seed(seed) 
     torch.manual_seed(seed) 
     random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
+
 
 
 
